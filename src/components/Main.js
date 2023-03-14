1111ea5a217276515713c5166643cbb3b8229c23
Yurii Dereguzov <DereguzovYE@yandex.ru>
2023-03-14 14:49:38 +0500

    refactor: Почистить компонент Main от лишнего закоментированного кода

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,37 +1,10 @@
 import React from "react";
-// import { api } from "../utils.js/Api";
 import Card from "./Card";
 import { CurrentUserContext } from '../contexts/CurrentUserContext';
 
 function Main({ cards, onEditProfile, onEditAvatar, onAddPlace, onCardClick, onCardLike, onCardDelete }) {
   // Подписываемся на контекст CurrentUserContext
   const currentUser = React.useContext(CurrentUserContext);
-
-  // const [cards, setCards] = React.useState([]);
-  // React.useEffect(() => {
-  //   api.getInitialCards()
-  //     .then((cardList) => {
-  //       setCards(cardList);
-  //     })
-  //     .catch((err) => {
-  //       console.log(`Ошибка: ${err}`); // выведем ошибку в консоль
-  //     });
-  // }, []);
-  // const [userName, setUserName] = React.useState("");
-  // const [userDescription, setUserDescription] = React.useState("");
-  // const [userAvatar, setUserAvatar] = React.useState("");
-  // React.useEffect(() => {
-  //   Promise.all([api.getProfile(), api.getInitialCards()])
-  //     .then(([userData, cardList]) => {
-  //       setUserName(userData.name);
-  //       setUserDescription(userData.about);
-  //       setUserAvatar(userData.avatar);
-  //       setCards(cardList);
-  //     })
-  //     .catch((err) => {
-  //       console.log(`Ошибка: ${err}`); // выведем ошибку в консоль
-  //     });
-  // }, []);
 
   return (
     <main>
