3cf957eb901c3e54ee4c8ee028b266f7863b6391
Yurii Dereguzov <DereguzovYE@yandex.ru>
2023-03-16 23:28:44 +0500

    feat: подключить бургер меню

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,14 @@
-import React from "react";
+import { React, useState } from "react";
 import logo from "../images/Vector-logo.svg";
 import { Link } from "react-router-dom";
+import CloseIcon from "../images/CloseIcon.svg"
 
 function Header(props) {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  function handleClick() {
+    setIsMenuOpen(!isMenuOpen);
+  }
+  
   function signOut() {
     localStorage.removeItem("jwt");
   }
@@ -11,13 +17,27 @@ function Header(props) {
     <header className="header">
       <div className="header__components">
         <img className="header__logo" src={logo} alt="Место Россия" />
-        <button className="header__menu-btn">
-          <span className="header__span"></span>
-          <span className="header__span"></span>
-          <span className="header__span"></span>
+        <button onClick={handleClick} className="header__menu-btn">
+          {isMenuOpen ? (
+            <img src={CloseIcon} alt="Крест" className="header__menu-btn" />
+          ) : (
+            <>
+              <span className="header__span"></span>
+              <span className="header__span"></span>
+              <span className="header__span"></span>
+            </>
+          )}
         </button>
+        {/* <button onClick={handleClick} className="header__menu-btn" style={{ backgroundImage: `url(${CloseIcon})` }}>
+          <span className="header__span"></span>
+          <span className="header__span"></span>
+          <span className="header__span"></span>
+        </button> */}
       </div>
-      <div className="header__loged">
+      {/* <div className="header__loged"> */}
+      <div
+        className={`header__loged ${isMenuOpen ? "header__loged_opened" : ""}`}
+      >
         <p className="heder__email">{props.email}</p>
         <button onClick={signOut} className="header__button">
           <Link to={props.link} className="header__link">
